00663ae8d0d3a877622ae9aa712ecf29f6806884
JordyV23 <jordyvargas0123@gmail.com>
2023-09-24 19:23:26 -0600

    Limpieza de proyecto y Configuracion de pinia

---
+++ src/counter/pages/Counter1Page.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>Counter 1 Page</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import CounterPage from '@/counter/pages/Counter1Page.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'counter1',
+      component: CounterPage
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+
   ]
 })
 
