77d51ad6245b6228a85f5fb560a29396b776b1e5
JordyV23 <jordyvargas0123@gmail.com>
2023-09-28 22:15:43 -0600

    Estructuracion de components

--- src/clients/pages/ListPage.vue
+++ src/clients/pages/ListPage.vue
@@ -1,11 +1,22 @@
 
 <script setup lang="ts">
+import ClientsList from '@/clients/components/ClientsList.vue';
+
 
 </script>
 
 <template>
     <div>
         <h1>Listado de Clients</h1>
+
+        <!-- Clinet List -->
+        <ClientsList/>
+
+        <!-- Pagination Numbers -->
+
+        <!-- Loading Modal -->
+
+        
     </div>
 </template>
 
