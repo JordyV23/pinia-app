29a4cba5722ddd0bff2ec00bac41635d662880f1
JordyV23 <jordyvargas0123@gmail.com>
2023-10-01 17:14:26 -0600

    Refactorizacion de componente

--- src/clients/composables/useClient.ts
+++ src/clients/composables/useClient.ts
@@ -1,6 +1,6 @@
-import { ref, watch } from "vue";
+import { useQuery, useMutation } from "@tanstack/vue-query";
+import { computed, ref, watch } from "vue";
 import type { Client } from "../interfaces/client";
-import { useQuery } from "@tanstack/vue-query";
 import clientsApi from "@/api/clients-api";
 
 const getClient = async (id: number): Promise<Client> => {
@@ -8,20 +8,32 @@ const getClient = async (id: number): Promise<Client> => {
   return data;
 };
 
+//Se nueve
+const updateClient = async (client: Client): Promise<Client> => {
+  //Particularidad: Validar que no se pueda modificar el ID es parte del backend
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { id, ...cliente } = client;
+  const { data } = await clientsApi.patch(`clients/${client.id}`, cliente);
+  return data;
+};
+
 const useClient = (id: number) => {
   const client = ref<Client>();
 
+  //Se mueve
+  const clientMutation = useMutation(updateClient);
+
   const { isLoading, data, isError } = useQuery(
-    ["client", id], 
+    ["client", id],
     () => getClient(id),
-    {retry: false,},
+    { retry: false }
   );
 
   watch(
     data,
     () => {
       if (data.value) {
-        client.value = {...data.value};
+        client.value = { ...data.value };
       }
     },
     { immediate: true }
@@ -31,6 +43,12 @@ const useClient = (id: number) => {
     isLoading,
     client,
     isError,
+    clientMutation,
+
+    //Metodos
+    hasUpdatingError: computed(() => clientMutation.isError),
+    isUpdatingClient: computed(() => clientMutation.isLoading.value),
+    hasUpdatedSuccess: computed(() => clientMutation.isSuccess.value)
   };
 };
 
--- src/clients/pages/ClientsPage.vue
+++ src/clients/pages/ClientsPage.vue
@@ -1,52 +1,41 @@
 <script setup lang="ts">
 //Primero importaciones de Terceros
-import { useMutation, useQueryClient } from "@tanstack/vue-query";
 import { useRoute, useRouter } from "vue-router";
 //Luego modulos internos
 import useClient from "@/clients/composables/useClient";
 import LoadingModal from "@/shared/components/LoadingModal.vue";
-import type { Client } from "@/clients/interfaces/client";
-import clientsApi from "@/api/clients-api";
 import { watch } from "vue";
 
-const route = useRoute();
-const router = useRouter();
-const queryClient = useQueryClient();
+const route = useRoute(); //Se queda
+const router = useRouter(); //Se queda
 
-const { client, isLoading, isError } = useClient(+route.params.id);
-
-const updateClient = async (client: Client): Promise<Client> => {
-  //Particularidad: Validar que no se pueda modificar el ID es parte del backend
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { id, ...cliente } = client;
-
-  const { data } = await clientsApi.patch(`clients/${client.id}`, cliente);
-  const queries = queryClient
-    .getQueryCache()
-    .findAll(["clients?page="], { exact: false });
-  queries.forEach((query) => query.fetch());
-
-  return data;
-};
-
-const clientMutation = useMutation(updateClient);
+const {
+  client,
+  isLoading,
+  isError,
+  clientMutation,
+  isUpdatingClient,
+  hasUpdatedSuccess,
+} = useClient(+route.params.id);
 
 watch(clientMutation.isSuccess, () => {
   setTimeout(() => {
     clientMutation.reset();
+
+    router.push("/clients");
   });
 });
 
 watch(isError, () => {
-  if(isError){
-    router.replace('/clients')
+  if (isError) {
+    router.replace("/clients");
   }
-})
+});
 </script>
 
 <template>
-  <h3 v-if="clientMutation.isLoading.value">Guardando...</h3>
-  <h3 v-if="clientMutation.isSuccess.value">Guardado</h3>
+  <h3 v-if="isUpdatingClient">Guardando...</h3>
+  <h3 v-if="hasUpdatedSuccess">Guardado</h3>
 
   <LoadingModal v-if="isLoading" />
 
